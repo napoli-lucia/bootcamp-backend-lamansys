fde36df4c2a155ce35c736dbf58effab95f43fa1
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-16 17:46:07 -0300

    EC-22: Creo entidad Product

---
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/entity/Product.java
@@ -0,0 +1,34 @@
+package ar.lamansys.messages.infrastructure.output.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "product")
+@Entity
+public class Product {
+
+    //Como generar automatico el id con string?
+    @Id
+    @Column(name = "product_id")
+    private String productId;
+
+    //FK
+    @Column(name = "owner_id", nullable = false)
+    private String ownerId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    @Column(name = "unity_price", nullable = false)
+    private Float unityPrice;
+
+}
