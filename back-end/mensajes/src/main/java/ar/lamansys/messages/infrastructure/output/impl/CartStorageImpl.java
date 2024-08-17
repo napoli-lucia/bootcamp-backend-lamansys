ff823bd3406ca0cc584265e96cbbdf1388099cc6
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-17 18:30:58 -0300

    EC-37: Creo la clase CartStorageImpl

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/impl/CartStorageImpl.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/impl/CartStorageImpl.java
@@ -1,2 +1,28 @@
-package ar.lamansys.messages.infrastructure.output.impl;public class CartStorageImpl {
+package ar.lamansys.messages.infrastructure.output.impl;
+
+import ar.lamansys.messages.domain.CartBo;
+import ar.lamansys.messages.infrastructure.output.CartStorage;
+import ar.lamansys.messages.infrastructure.output.entity.Cart;
+import ar.lamansys.messages.infrastructure.output.entity.Product;
+import ar.lamansys.messages.infrastructure.output.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@RequiredArgsConstructor
+@Service
+public class CartStorageImpl implements CartStorage {
+
+    private final CartRepository cartRepository;
+
+    @Override
+    public void save(CartBo cart) {
+        cartRepository.save(new Cart(cart));
+    }
+
+    @Override
+    public void delete(String cartId) {
+        cartRepository.deleteById(cartId);
+    }
 }
