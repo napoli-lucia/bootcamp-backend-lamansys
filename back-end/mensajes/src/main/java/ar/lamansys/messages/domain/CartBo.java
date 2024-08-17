f1e2a8949392d401d4a6a4e44364feccd612a724
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-17 18:32:04 -0300

    EC-39: Creo la clase CartBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/CartBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/CartBo.java
@@ -1,2 +1,10 @@
-package ar.lamansys.messages.domain;public class CartBo {
+package ar.lamansys.messages.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@AllArgsConstructor
+@Value
+public class CartBo {
+    String cartId;
 }
