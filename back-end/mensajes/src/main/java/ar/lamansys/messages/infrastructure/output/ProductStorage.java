93c2aaec7d869e4f20f7c988ac5677d7c5f7debf
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-17 17:27:48 -0300

    EC-27: Creo la interfaz ProductStorage

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/ProductStorage.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/ProductStorage.java
@@ -1,2 +1,18 @@
-package ar.lamansys.messages.infrastructure.output;public interface ProductStorage {
+package ar.lamansys.messages.infrastructure.output;
+
+import ar.lamansys.messages.domain.ProductBo;
+import ar.lamansys.messages.infrastructure.output.entity.Product;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public interface ProductStorage {
+
+    void save(ProductBo product);
+
+    void delete(String productId);
+
+    Optional<Product> getProductByProductId(String productId);
+
+    Stream<ProductBo> getAllProductsByUserId(String userId);
 }
