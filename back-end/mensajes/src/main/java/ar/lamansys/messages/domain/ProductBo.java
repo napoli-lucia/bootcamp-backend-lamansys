7d5fe6ee754c011c4aa333ed80a676d8539eb9d7
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-17 17:29:05 -0300

    EC-30: Creo la clase ProductBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/ProductBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/ProductBo.java
@@ -1,2 +1,14 @@
-package ar.lamansys.messages.domain;public class ProductBo {
+package ar.lamansys.messages.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@AllArgsConstructor
+@Value
+public class ProductBo {
+    String productId;
+    String ownerId;
+    String productName;
+    Integer stock;
+    Float unityPrice;
 }
