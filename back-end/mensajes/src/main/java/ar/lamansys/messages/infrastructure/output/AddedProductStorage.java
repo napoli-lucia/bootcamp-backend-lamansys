e5a13c37809a6d14134933459c53282477e851bb
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-18 12:41:44 -0300

    EC-62: Creo interfaz AddedProductStorage

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/AddedProductStorage.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/AddedProductStorage.java
@@ -1,2 +1,19 @@
-package ar.lamansys.messages.infrastructure.output;public interface AddedProductStorage {
+package ar.lamansys.messages.infrastructure.output;
+
+import ar.lamansys.messages.domain.AddedProductBo;
+import ar.lamansys.messages.infrastructure.output.entity.AddedProductId;
+
+import java.util.stream.Stream;
+
+public interface AddedProductStorage {
+
+    void save(AddedProductBo addedProduct);
+
+    // Nos parece que no hace falta
+    //void modifyQuantity(AddedProductBo addedProduct);
+
+    void delete(AddedProductId addedProductId);
+
+    // TODO: obtener addedproduct por cartId
+    //Stream<AddedProductBo> getByCartId(String cartId);
 }
