3bf0fe55d8e231266e69a861747448127387b885
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-18 12:42:55 -0300

    EC-65: Creo clase AddedProductBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/AddedProductBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/AddedProductBo.java
@@ -1,2 +1,13 @@
-package ar.lamansys.messages.domain;public class AddedProductId {
+package ar.lamansys.messages.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@AllArgsConstructor
+@Value
+public class AddedProductBo {
+
+    String cartId;
+    String productId;
+    Integer quantity;
 }
