b9935757f4aa791db0fdaf4cb70e79b43f410acc
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-19 12:19:40 -0300

    EC-86: Creo la clase EditProductInCart e implemento su metodo run

---
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/EditProductInCart.java
@@ -0,0 +1,25 @@
+package ar.lamansys.messages.application;
+
+import ar.lamansys.messages.domain.AddedProductBo;
+import ar.lamansys.messages.domain.NewProductBo;
+import ar.lamansys.messages.infrastructure.output.AddedProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class EditProductInCart {
+
+    private final AddedProductStorage addedProduct;
+
+    public void run(String ownerId, String cartId, NewProductBo editedProduct) {
+
+        //TODO: Verificar que el carrito exista
+        //TODO: Verificar que el carrito sea del ownerId
+        //TODO: Verificar que el producto exista en el carrito
+        //TODO: Verificar que el stock sea suficiente
+
+        addedProduct.save(new AddedProductBo(cartId, editedProduct.getProductId(), editedProduct.getQuantity()));
+
+    }
+}
