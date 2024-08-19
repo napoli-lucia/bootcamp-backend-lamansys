c299626cdef42c6efe6d712e333619d63fb0cff8
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-19 11:00:16 -0300

    EC-78: Creo la clase CreateNewProductInCart e implemento su metodo run

---
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/CreateNewProductInCart.java
@@ -0,0 +1,29 @@
+package ar.lamansys.messages.application;
+
+import ar.lamansys.messages.domain.AddedProductBo;
+import ar.lamansys.messages.domain.NewProductBo;
+import ar.lamansys.messages.infrastructure.output.AddedProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CreateNewProductInCart {
+
+    private final AddedProductStorage addedProduct;
+    //private final CartStorage cartStorage;
+
+    public void run(String ownerId, String cartId, NewProductBo newProduct) {
+
+        //TODO: Verificar que el producto exista
+        //TODO: Verificar que el producto sea del mismo seller
+        //TODO: Verificar que el stock sea suficiente
+        //TODO: Verificar que el carrito exista
+        //TODO: Verificar que el carrito sea del ownerId
+
+        addedProduct.save(new AddedProductBo(cartId, newProduct.getProductId(), newProduct.getQuantity()));
+
+
+
+    }
+}
