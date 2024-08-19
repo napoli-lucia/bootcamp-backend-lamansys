7f9d493a43d5d064fd369506f066f107891abd3b
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-19 11:02:25 -0300

    EC-79: Creo el endpoint postNewProductInCart en CartController

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/input/rest/CartController.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/input/rest/CartController.java
@@ -1,9 +1,9 @@
 package ar.lamansys.messages.infrastructure.input.rest;
 
 import ar.lamansys.messages.application.CreateCart;
+import ar.lamansys.messages.application.CreateNewProductInCart;
 import ar.lamansys.messages.application.exception.UserNotExistsException;
 import ar.lamansys.messages.application.exception.UserSessionNotExists;
-import ar.lamansys.messages.domain.NewMessageBo;
 import ar.lamansys.messages.domain.NewProductBo;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +17,24 @@ import java.util.List;
 public class CartController {
 
     private final CreateCart createCart;
+    private final CreateNewProductInCart createNewProductInCart;
 
     @PostMapping("/{ownerId}/{sellerId}")
     public ResponseEntity postNewCart(@PathVariable String ownerId, @PathVariable String sellerId, @RequestBody List<NewProductBo> newProducts) throws UserNotExistsException, UserSessionNotExists {
         createCart.run(ownerId, sellerId, newProducts);
         return ResponseEntity.ok("Cart created successfully");
+    }
+
+    //ProductNotExistException
+    //CartNotExistException
+    //UserNotExistsException
+    //UserNotOwnerCartException
+    //ProductNotValidException -> excepcion valida que producto es el mismo del seller
+    //ProductStockNotEnough -> excepcion valida que el stock sea suficiente
+    @PostMapping("/addProduct/{ownerId}/{cartId}")
+    public ResponseEntity postNewProductInCart(@PathVariable String ownerId, @PathVariable String cartId, @RequestBody NewProductBo newProduct) {
+        createNewProductInCart.run(ownerId, cartId, newProduct);
+        return ResponseEntity.ok("Product added to cart successfully");
     }
 
 }
