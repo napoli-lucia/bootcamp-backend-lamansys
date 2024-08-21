061e1405da4b6beee82a9a3458fac2744d064979
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-21 10:17:39 -0300

    EC-100: Creo la clase ProductStateBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/product/ProductStateBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/product/ProductStateBo.java
@@ -1,2 +1,11 @@
-package ar.lamansys.messages.domain.product;public class ProductStateBo {
+package ar.lamansys.messages.domain.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@AllArgsConstructor
+@Value
+public class ProductStateBo {
+    String productId;
+    Float unityPrice;
 }
