21f0f5a37ded47308666ad11b5a3c84b4e728375
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-21 10:21:38 -0300

    Se modifican importaciones

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/entity/AddedProductId.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/entity/AddedProductId.java
@@ -1,9 +1,6 @@
 package ar.lamansys.messages.infrastructure.output.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
