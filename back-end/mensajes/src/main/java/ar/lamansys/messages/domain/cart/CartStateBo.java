35a4a5d344b0d33ab8d56535ea1eaf5b13d7c8ed
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-21 10:17:08 -0300

    EC-99: Creo la clase CartStateBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/cart/CartStateBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/cart/CartStateBo.java
@@ -1,2 +1,15 @@
-package ar.lamansys.messages.domain.cart;public class CartStateBo {
+package ar.lamansys.messages.domain.cart;
+
+import ar.lamansys.messages.domain.product.ProductStateBo;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import java.util.List;
+
+@AllArgsConstructor
+@Value
+public class CartStateBo {
+    Float totalPrice;
+    List<String> leftOut;
+    List<ProductStateBo> products;
+
 }
