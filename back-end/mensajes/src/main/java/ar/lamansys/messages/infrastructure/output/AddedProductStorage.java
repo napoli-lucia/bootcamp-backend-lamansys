91650b387afc55f9cc9615a4328c14ca326e8bac
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-21 10:18:25 -0300

    EC-101: Creo el metodo findAllByCartId en la clase AddedProductStorage

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/AddedProductStorage.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/output/AddedProductStorage.java
@@ -1,12 +1,18 @@
 package ar.lamansys.messages.infrastructure.output;
 
 import ar.lamansys.messages.domain.addedproduct.AddedProductBo;
+import ar.lamansys.messages.infrastructure.output.entity.AddedProduct;
 import ar.lamansys.messages.infrastructure.output.entity.AddedProductId;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public interface AddedProductStorage {
 
     void save(AddedProductBo addedProduct);
 
     void delete(AddedProductId addedProductId);
+
+    List<AddedProduct> findAllByCartId(String cartId);
 
 }
