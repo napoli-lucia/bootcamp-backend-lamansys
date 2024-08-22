082d3b202cb23355e5d276f3ef40c0992d83dcdb
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:32:48 -0300

    EC-119: Organizo las excepciones en el MessageExceptionHandler

--- back-end/mensajes/src/main/java/ar/lamansys/messages/MessageExceptionHandler.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/MessageExceptionHandler.java
@@ -1,5 +1,6 @@
 package ar.lamansys.messages;
 
+import ar.lamansys.messages.application.cart.exception.*;
 import ar.lamansys.messages.application.user.exception.UserExistsException;
 import ar.lamansys.messages.application.user.exception.UserNotExistsException;
 import ar.lamansys.messages.application.user.exception.UserSessionNotExists;
@@ -8,18 +9,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class MessageExceptionHandler {
 
-    @ExceptionHandler({UserNotExistsException.class, UserSessionNotExists.class})
+    @ExceptionHandler(MultipleExceptions.class)
+    public ResponseEntity<List<String>> multipleExceptionsHandler(MultipleExceptions ex) {
+        List<String> exceptionMessages = ex.getExceptions().stream()
+                .map(Throwable::getLocalizedMessage)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(400).body(exceptionMessages);
+    }
+
+    @ExceptionHandler({UserNotExistsException.class, UserSessionNotExists.class, CartNotExistsException.class, ProductNotExistsException.class})
     public ResponseEntity<String> notExistsHandler(Exception ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getLocalizedMessage());
     }
 
-    @ExceptionHandler(UserExistsException.class)
+    @ExceptionHandler({UserExistsException.class, ProductStockNotEnough.class, ProductInvalidSeller.class, ProductNotExistsInCartException.class})
     public ResponseEntity<String> existsHandler(Exception ex){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getLocalizedMessage());
     }
 
+    @ExceptionHandler(UserNotOwnsCartException.class)
+    public ResponseEntity<String> unauthorizedHandler(Exception ex){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getLocalizedMessage());
+    }
 
 }
