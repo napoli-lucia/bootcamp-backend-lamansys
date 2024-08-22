a6e2391383466f80806f0c84ec9e46454559e558
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:29:46 -0300

    EC-115: Creo la excepcion ProductStockNotEnough

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/ProductStockNotEnough.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/ProductStockNotEnough.java
@@ -1,2 +1,14 @@
-package ar.lamansys.messages.application.cart.exception;public class ProductStockNotEnough {
+package ar.lamansys.messages.application.cart.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ProductStockNotEnough extends Exception{
+
+    String productId;
+
+    public ProductStockNotEnough(String productId) {
+        super(String.format("Product %s cannot meet the stock", productId));
+        this.productId = productId;
+    }
 }
