edac434960cfb1dd5d2a682489b593f0ce4d901b
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:25:01 -0300

    EC-109: Creo la excepcion CartNotExistsException

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/CartNotExistsException.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/CartNotExistsException.java
@@ -1,2 +1,13 @@
-package ar.lamansys.messages.application.cart.exception;public class CartNotExistsException {
+package ar.lamansys.messages.application.cart.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CartNotExistsException extends Exception{
+    String cartId;
+
+    public CartNotExistsException(String cartId) {
+        super(String.format("Cart %s not exists", cartId));
+        this.cartId = cartId;
+    }
 }
