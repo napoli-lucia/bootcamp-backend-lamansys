883719e502bdf2127069008c0bd6ec5e1d0db1c3
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:41:30 -0300

    EC-131: Agrego las excepciones en CartController

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/input/rest/cart/CartController.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/input/rest/cart/CartController.java
@@ -1,9 +1,10 @@
 package ar.lamansys.messages.infrastructure.input.rest.cart;
 
 import ar.lamansys.messages.application.cart.*;
+import ar.lamansys.messages.application.cart.exception.*;
 import ar.lamansys.messages.application.user.exception.UserNotExistsException;
-import ar.lamansys.messages.application.user.exception.UserSessionNotExists;
 import ar.lamansys.messages.domain.addedproduct.NewProductBo;
+import ar.lamansys.messages.domain.cart.CartCreationBo;
 import ar.lamansys.messages.domain.cart.CartStateBo;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,37 +24,43 @@ public class CartController {
     private final GetCartState getCartState;
 
     @PostMapping("/{ownerId}/{sellerId}")
-    public ResponseEntity postNewCart(@PathVariable String ownerId, @PathVariable String sellerId, @RequestBody List<NewProductBo> newProducts) throws UserNotExistsException, UserSessionNotExists {
-        createCart.run(ownerId, sellerId, newProducts);
-        return ResponseEntity.ok("Cart created successfully");
+    public CartCreationBo postNewCart(@PathVariable String ownerId,
+                                      @PathVariable String sellerId,
+                                      @RequestBody List<NewProductBo> newProducts) throws MultipleExceptions, UserNotExistsException {
+        return createCart.run(ownerId, sellerId, newProducts);
+        //return ResponseEntity.ok("Cart created successfully");
     }
 
-    //ProductNotExistException
-    //CartNotExistException
-    //UserNotExistsException
-    //UserNotOwnerCartException
-    //ProductNotValidException -> excepcion valida que producto es el mismo del seller
-    //ProductStockNotEnough -> excepcion valida que el stock sea suficiente
     @PostMapping("/addProduct/{ownerId}/{cartId}")
-    public ResponseEntity postNewProductInCart(@PathVariable String ownerId, @PathVariable String cartId, @RequestBody NewProductBo newProduct) {
+    public ResponseEntity postNewProductInCart(@PathVariable String ownerId,
+                                               @PathVariable String cartId,
+                                               @RequestBody NewProductBo newProduct) throws
+            UserNotExistsException, CartNotExistsException, UserNotOwnsCartException, ProductNotExistsException, ProductInvalidSeller, ProductStockNotEnough {
         createNewProductInCart.run(ownerId, cartId, newProduct);
         return ResponseEntity.ok("Product added to cart successfully");
     }
 
     @PutMapping("/editProduct/{ownerId}/{cartId}")
-    public ResponseEntity editProductInCart(@PathVariable String ownerId, @PathVariable String cartId, @RequestBody NewProductBo editedProduct) {
+    public ResponseEntity editProductInCart(@PathVariable String ownerId,
+                                            @PathVariable String cartId,
+                                            @RequestBody NewProductBo editedProduct) throws
+            CartNotExistsException, UserNotExistsException, UserNotOwnsCartException, ProductNotExistsInCartException, ProductStockNotEnough {
         editProductInCart.run(ownerId, cartId, editedProduct);
         return ResponseEntity.ok("Product edited quantity in cart successfully");
     }
 
     @DeleteMapping("/deleteProduct/{ownerId}/{cartId}/{productId}")
-    public ResponseEntity deleteProductInCart(@PathVariable String ownerId, @PathVariable String cartId, @PathVariable String productId) {
+    public ResponseEntity deleteProductInCart(@PathVariable String ownerId,
+                                              @PathVariable String cartId,
+                                              @PathVariable String productId) throws
+            CartNotExistsException, UserNotExistsException, UserNotOwnsCartException, ProductNotExistsInCartException {
         deleteProductInCart.run(ownerId, cartId, productId);
         return ResponseEntity.ok("Product in cart deleted successfully");
     }
 
     @GetMapping("/state/{ownerId}/{cartId}")
-    public CartStateBo getCartState(@PathVariable String ownerId, @PathVariable String cartId) {
+    public CartStateBo getCartState(@PathVariable String ownerId, @PathVariable String cartId) throws
+            CartNotExistsException, UserNotExistsException, UserNotOwnsCartException {
         return getCartState.run(ownerId, cartId);
     }
 
