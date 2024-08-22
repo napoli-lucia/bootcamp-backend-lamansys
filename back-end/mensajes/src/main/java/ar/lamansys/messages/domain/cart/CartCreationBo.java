1821e923f3cf754f349a8e33805fd32789080265
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:40:19 -0300

    EC-130: Creo la clase CartCreationBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/cart/CartCreationBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/cart/CartCreationBo.java
@@ -1,2 +1,15 @@
-package ar.lamansys.messages.domain.cart;public class CartCreationBo {
+package ar.lamansys.messages.domain.cart;
+
+import ar.lamansys.messages.domain.addedproduct.NewProductBo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CartCreationBo {
+    private String cartId;
+    private List<NewProductBo> addedProducts;
+    private List<String> exceptions;
 }
