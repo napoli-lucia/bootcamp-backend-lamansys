4dcfb815ade1322067be66b6163245e606689db9
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:37:29 -0300

    EC-126: Agrego assertions al caso de uso CreateNewProductInCart

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/CreateNewProductInCart.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/CreateNewProductInCart.java
@@ -1,5 +1,8 @@
 package ar.lamansys.messages.application.cart;
 
+import ar.lamansys.messages.application.cart.exception.*;
+import ar.lamansys.messages.application.user.AssertUserExists;
+import ar.lamansys.messages.application.user.exception.UserNotExistsException;
 import ar.lamansys.messages.domain.addedproduct.AddedProductBo;
 import ar.lamansys.messages.domain.addedproduct.NewProductBo;
 import ar.lamansys.messages.infrastructure.output.AddedProductStorage;
@@ -11,15 +14,23 @@ import org.springframework.stereotype.Service;
 public class CreateNewProductInCart {
 
     private final AddedProductStorage addedProduct;
-    //private final CartStorage cartStorage;
+    private final AssertUserExists assertUserExists;
+    private final AssertCartExists assertCartExists;
+    private final AssertUserOwnsCart assertUserOwnsCart;
+    private final AssertProductExists assertProductExists;
+    private final AssertProductValidSeller assertProductValidSeller;
+    private final AssertProductStockEnough assertProductStockEnough;
 
-    public void run(String ownerId, String cartId, NewProductBo newProduct) {
+    public void run(String ownerId, String cartId, NewProductBo newProduct) throws
+            UserNotExistsException, CartNotExistsException, UserNotOwnsCartException,
+            ProductNotExistsException, ProductInvalidSeller, ProductStockNotEnough {
 
-        //TODO: Verificar que el producto exista
-        //TODO: Verificar que el producto sea del mismo seller
-        //TODO: Verificar que el stock sea suficiente
-        //TODO: Verificar que el carrito exista
-        //TODO: Verificar que el carrito sea del ownerId
+        assertUserExists.run(ownerId);
+        assertCartExists.run(cartId);
+        assertUserOwnsCart.run(cartId, ownerId);
+        assertProductExists.run(newProduct.getProductId());
+        assertProductValidSeller.run(cartId, newProduct.getProductId());
+        assertProductStockEnough.run(newProduct.getProductId(), newProduct.getQuantity());
 
         addedProduct.save(new AddedProductBo(cartId, newProduct.getProductId(), newProduct.getQuantity()));
 
