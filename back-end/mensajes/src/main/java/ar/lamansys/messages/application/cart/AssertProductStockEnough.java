8382701096687f7161eea38c990accf9266f56b8
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:22:11 -0300

    EC-114: Creo la assertion ProductStockEnough

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/AssertProductStockEnough.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/AssertProductStockEnough.java
@@ -1,2 +1,19 @@
-package ar.lamansys.messages.application.cart;public class AssertProductStockEnough {
+package ar.lamansys.messages.application.cart;
+
+import ar.lamansys.messages.application.cart.exception.ProductStockNotEnough;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class AssertProductStockEnough {
+
+    private ProductStorage productStorage;
+
+    public void run(String productId, int quantity) throws ProductStockNotEnough {
+        if ( productStorage.getStockByProductId(productId) < quantity) {
+            throw new ProductStockNotEnough(productId);
+        }
+    }
 }
