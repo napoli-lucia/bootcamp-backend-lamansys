04f5775e806a0cc7e0474da73c345100eaffafab
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:36:52 -0300

    EC-125: Agrego assertions al caso de uso CreateCart

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/CreateCart.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/CreateCart.java
@@ -1,15 +1,24 @@
 package ar.lamansys.messages.application.cart;
 
+import ar.lamansys.messages.application.cart.exception.MultipleExceptions;
+import ar.lamansys.messages.application.cart.exception.ProductInvalidSeller;
+import ar.lamansys.messages.application.cart.exception.ProductNotExistsException;
+import ar.lamansys.messages.application.cart.exception.ProductStockNotEnough;
+import ar.lamansys.messages.application.user.AssertUserExists;
+import ar.lamansys.messages.application.user.exception.UserNotExistsException;
 import ar.lamansys.messages.domain.addedproduct.AddedProductBo;
 import ar.lamansys.messages.domain.cart.CartBo;
 import ar.lamansys.messages.domain.addedproduct.NewProductBo;
+import ar.lamansys.messages.domain.cart.CartCreationBo;
 import ar.lamansys.messages.infrastructure.output.AddedProductStorage;
 import ar.lamansys.messages.infrastructure.output.CartStorage;
 import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import ar.lamansys.messages.infrastructure.output.entity.Product;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.UUID;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -18,16 +27,67 @@ public class CreateCart {
     private final ProductStorage productStorage;
     private final AddedProductStorage addedProduct;
     private final CartStorage cartStorage;
+    private final AssertUserExists assertUserExists;
+    private final AssertProductStockEnough assertProductStockEnough;
+    private final AssertProductExists assertProductExists;
+    private final AssertProductSameSeller assertProductSameSeller;
 
-    public void run(String ownerId, String sellerId, List<NewProductBo> newProducts) {
+    public CartCreationBo run(String ownerId, String sellerId, List<NewProductBo> newProducts) throws MultipleExceptions, UserNotExistsException {
 
-        String cartId = UUID.randomUUID().toString();
+        List<Exception> exceptions = new ArrayList<>();
 
-        cartStorage.save(new CartBo(cartId, ownerId, sellerId));
+        assertUserExists.run(ownerId);
+        assertUserExists.run(sellerId);
+
+        List<NewProductBo> availableProducts = new ArrayList<>();
 
         for (NewProductBo newProduct : newProducts) {
+
+            try {
+                assertProductExists.run(newProduct.getProductId());
+            } catch (ProductNotExistsException e) {
+                exceptions.add(e);
+            }
+
+            try {
+                if(productStorage.exists(newProduct.getProductId())) assertProductSameSeller.run(sellerId, newProduct.getProductId());
+            } catch (ProductInvalidSeller e) {
+                exceptions.add(e);
+            }
+
+            try {
+                Optional<Product> product = productStorage.getProductByProductId(newProduct.getProductId());
+                if(product.isPresent()){
+                    assertProductStockEnough.run(newProduct.getProductId(), newProduct.getQuantity());
+                    availableProducts.add(newProduct);
+                }
+
+            } catch (ProductStockNotEnough e) {
+                exceptions.add(e);
+            }
+        }
+
+        //Agrego productos disponibles
+        String cartId = UUID.randomUUID().toString();
+        cartStorage.save(new CartBo(cartId, ownerId, sellerId));
+
+        for (NewProductBo newProduct : availableProducts) {
             addedProduct.save(new AddedProductBo(cartId, newProduct.getProductId(), newProduct.getQuantity()));
         }
+
+        if (!exceptions.isEmpty() && availableProducts.isEmpty()){
+            throw new MultipleExceptions(exceptions);
+        }
+
+        List<String> exceptionMessages = exceptions.stream()
+                .map(Throwable::getLocalizedMessage)
+                .collect(Collectors.toList());
+
+        if (!exceptions.isEmpty()){
+            return new CartCreationBo(cartId, availableProducts, exceptionMessages);
+        }
+
+        return new CartCreationBo(cartId, availableProducts, Collections.emptyList());
 
     }
 }
