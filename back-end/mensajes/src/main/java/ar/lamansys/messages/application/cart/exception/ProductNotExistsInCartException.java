e6ef0aafc518c6be8af0c8bad7cc3fd2f9994ec8
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:29:13 -0300

    EC-121: Creo la excepcion ProductNotExistsInCartException

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/ProductNotExistsInCartException.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/ProductNotExistsInCartException.java
@@ -3,12 +3,12 @@ package ar.lamansys.messages.application.cart.exception;
 import lombok.Getter;
 
 @Getter
-public class ProductNotExistsException extends Exception{
+public class ProductNotExistsInCartException extends Exception{
 
     String productId;
 
-    public ProductNotExistsException(String productId) {
-        super(String.format("Product %s not exists", productId));
+    public ProductNotExistsInCartException(String productId) {
+        super(String.format("Product %s not exists in cart", productId));
         this.productId = productId;
     }
 }
