a6d323583ffe263b5911f4a4699428b82601d586
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:30:34 -0300

    EC-111: Creo la excepcion UserNotOwnsCartException

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/UserNotOwnsCartException.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/UserNotOwnsCartException.java
@@ -1,2 +1,16 @@
-package ar.lamansys.messages.application.cart.exception;public class UserNotOwnsCartException {
+package ar.lamansys.messages.application.cart.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UserNotOwnsCartException extends Exception{
+
+    String cartId;
+    String ownerId;
+
+    public UserNotOwnsCartException(String cartId, String ownerId) {
+        super(String.format("Owner %s does not own cart %s", ownerId, cartId));
+        this.cartId = cartId;
+        this.ownerId = ownerId;
+    }
 }
