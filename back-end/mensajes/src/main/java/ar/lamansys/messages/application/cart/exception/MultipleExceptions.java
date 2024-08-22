9c845244ab8afbfaf573276ad1c94e69140d5169
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:25:59 -0300

    EC-123: Creo la excepcion MultipleExceptions

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/MultipleExceptions.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/MultipleExceptions.java
@@ -1,2 +1,15 @@
-package ar.lamansys.messages.application.cart.exception;public class MultipleExceptions {
+package ar.lamansys.messages.application.cart.exception;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public class MultipleExceptions extends Exception{
+    private final List<Exception> exceptions;
+
+    public MultipleExceptions(List<Exception> exceptions) {
+        super("Multiple exceptions occurred");
+        this.exceptions = exceptions;
+    }
+
 }
