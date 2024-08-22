eace173b47a6e6d2daeb22a6a389106a4251d524
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:27:56 -0300

    EC-117: Creo la excepcion ProductInvalidSeller

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/ProductInvalidSeller.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/exception/ProductInvalidSeller.java
@@ -1,2 +1,14 @@
-package ar.lamansys.messages.application.cart.exception;public class ProductInvalidSeller {
+package ar.lamansys.messages.application.cart.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ProductInvalidSeller extends Exception{
+
+    String productId;
+
+    public ProductInvalidSeller(String productId) {
+        super(String.format("Product %s has an invalid seller", productId));
+        this.productId = productId;
+    }
 }
