3d1b39df326ba0569a24b6f0a5e89dbed580b3fc
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-22 14:23:25 -0300

    EC-120: Creo la assertion ProductExistsInCart

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/AssertProductExistsInCart.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/AssertProductExistsInCart.java
@@ -1,19 +1,22 @@
 package ar.lamansys.messages.application.cart;
 
 import ar.lamansys.messages.application.cart.exception.ProductNotExistsException;
+import ar.lamansys.messages.application.cart.exception.ProductNotExistsInCartException;
+import ar.lamansys.messages.infrastructure.output.AddedProductStorage;
 import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import ar.lamansys.messages.infrastructure.output.entity.AddedProductId;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
-public class AssertProductExists {
+public class AssertProductExistsInCart {
 
-    private ProductStorage productStorage;
+    private AddedProductStorage addedProductStorage;
 
-    public void run(String productId) throws ProductNotExistsException {
-        if ( !productStorage.exists(productId) ) {
-            throw new ProductNotExistsException(productId);
+    public void run(String cartId, String productId) throws ProductNotExistsInCartException {
+        if ( !addedProductStorage.exists(new AddedProductId(cartId, productId) )) {
+            throw new ProductNotExistsInCartException(productId);
         }
     }
 }
