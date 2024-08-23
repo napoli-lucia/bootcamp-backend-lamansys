124a66b7c2bc1b09380128de8b77ca1e08e118b2
Lucia Napoli <lucianapoli15@gmail.com>
2024-08-23 18:52:35 -0300

    EC-142: Creo la prueba unitaria AssertProductExistsTest

---
+++ back-end/mensajes/src/test/java/ar/lamansys/messages/application/cart/AssertProductExistsTest.java
@@ -0,0 +1,28 @@
+package ar.lamansys.messages.application.cart;
+
+import ar.lamansys.messages.application.cart.exception.ProductNotExistsException;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AssertProductExistsTest {
+
+    private ProductStorage productStorage = Mockito.mock(ProductStorage.class);
+
+    private AssertProductExists assertProductExists = new AssertProductExists(productStorage);
+
+    @Test
+    void productExistence_ok() {
+        Mockito.when(productStorage.exists("productId")).thenReturn(true);
+        assertDoesNotThrow(() -> assertProductExists.run("productId"));
+    }
+
+    @Test
+    void productNotExistence_throwsException() {
+        Mockito.when(productStorage.exists("productId")).thenReturn(false);
+        assertThrows(ProductNotExistsException.class, () -> assertProductExists.run("productId"));
+    }
+}
