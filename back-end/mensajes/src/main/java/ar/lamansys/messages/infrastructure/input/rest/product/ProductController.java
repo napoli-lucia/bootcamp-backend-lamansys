72411a4a8ba031208d47fb1456f8ce5a7b1fa49a
Emiliana Girardi <girardiemiliana@gmail.com>
2024-09-03 16:19:18 -0300

    EC-166: creo el endpoint postNewProduct en ProductController

--- back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/input/rest/product/ProductController.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/infrastructure/input/rest/product/ProductController.java
@@ -1,13 +1,14 @@
 package ar.lamansys.messages.infrastructure.input.rest.product;
 
+import ar.lamansys.messages.application.product.AddProduct;
 import ar.lamansys.messages.application.product.ListProducts;
 import ar.lamansys.messages.application.user.exception.UserNotExistsException;
+import ar.lamansys.messages.domain.addedproduct.NewProductBo;
+import ar.lamansys.messages.domain.product.PostProductBo;
 import ar.lamansys.messages.domain.product.ProductBo;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +17,17 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
     private final ListProducts listProducts;
+    private final AddProduct addProduct;
 
     @GetMapping("/{ownerId}")
     public List<ProductBo> getAllProductsByOwnerId(@PathVariable String ownerId) throws UserNotExistsException {
         return listProducts.run(ownerId);
     }
+
+    @PostMapping ()
+    public ResponseEntity<String> getAllProductsByOwnerId(@RequestBody PostProductBo newProduct) throws UserNotExistsException {
+        addProduct.run(newProduct);
+        return ResponseEntity.ok("Product added successfully");
+    }
+
 }
