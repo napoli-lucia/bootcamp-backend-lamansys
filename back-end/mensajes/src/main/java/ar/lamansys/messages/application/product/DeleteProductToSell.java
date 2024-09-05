5a0f87ac9efa644605953d8be016f20cfe242bb4
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 10:52:52 -0300

    EC-184: Creo la clase DeleteProductToSell e implemento su metodo run

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/DeleteProductToSell.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/DeleteProductToSell.java
@@ -1,2 +1,19 @@
-package ar.lamansys.messages.application.product;public class DeleteProductToSell {
+package ar.lamansys.messages.application.product;
+
+import ar.lamansys.messages.application.product.exception.ProductNotExistsException;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class DeleteProductToSell {
+    private final ProductStorage productStorage;
+    private final AssertProductExists assertProductExists;
+
+    public void run(String productId) throws ProductNotExistsException {
+        assertProductExists.run(productId);
+
+        productStorage.delete(productId);
+    }
 }
