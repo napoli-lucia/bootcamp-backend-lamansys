9690326a9e65410d5f2c5d04619d24760ea03b04
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 13:20:04 -0300

    EC-193: Agrego assert y exception en la clase DeleteProductToSell

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/DeleteProductToSell.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/DeleteProductToSell.java
@@ -1,6 +1,9 @@
 package ar.lamansys.messages.application.product;
 
+import ar.lamansys.messages.application.product.exception.InvalidSellerException;
 import ar.lamansys.messages.application.product.exception.ProductNotExistsException;
+import ar.lamansys.messages.application.user.AssertUserExists;
+import ar.lamansys.messages.application.user.exception.UserNotExistsException;
 import ar.lamansys.messages.infrastructure.output.ProductStorage;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +13,13 @@ import org.springframework.stereotype.Service;
 public class DeleteProductToSell {
     private final ProductStorage productStorage;
     private final AssertProductExists assertProductExists;
+    private final AssertUserExists assertUserExists;
+    private final AssertUserIsSeller assertUserSeller;
 
-    public void run(String productId) throws ProductNotExistsException {
+    public void run(String sellerId, String productId) throws ProductNotExistsException, UserNotExistsException, InvalidSellerException {
         assertProductExists.run(productId);
+        assertUserExists.run(sellerId);
+        assertUserSeller.run(sellerId, productStorage.getOwnerByProductId(productId));
 
         productStorage.delete(productId);
     }
