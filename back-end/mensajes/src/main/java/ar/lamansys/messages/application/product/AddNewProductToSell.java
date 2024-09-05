471bf98e8f778829fb1b78b11907e199e30850b0
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 00:52:25 -0300

    EC-178: Creo la clase AddNewProductToSell e implemento su metodo run

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AddNewProductToSell.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AddNewProductToSell.java
@@ -1,2 +1,26 @@
-package ar.lamansys.messages.application.product;public class AddNewProductToSell {
+package ar.lamansys.messages.application.product;
+
+import ar.lamansys.messages.application.product.exception.ProductExistsException;
+import ar.lamansys.messages.application.user.AssertUserExists;
+import ar.lamansys.messages.application.user.exception.UserNotExistsException;
+import ar.lamansys.messages.domain.addedproduct.NewProductBo;
+import ar.lamansys.messages.domain.product.ProductBo;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class AddNewProductToSell {
+    private final ProductStorage productStorage;
+    private final AssertUserExists assertUserExists;
+    private final AssertProductNotExists assertProductNotExists;
+
+    public void run(ProductBo newProduct) throws UserNotExistsException, ProductExistsException {
+        assertProductNotExists.run(newProduct.getProductId());
+        assertUserExists.run(newProduct.getOwnerId());
+
+        productStorage.save(newProduct);
+    }
 }
