c4d06c6e180bc6a22e89c5a8031c3ec3e94d4546
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 11:05:07 -0300

    EC-187: Creo la clase GetProductInfo e implemento su metodo run

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/GetProductInfo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/GetProductInfo.java
@@ -1,9 +1,23 @@
 package ar.lamansys.messages.application.product;
 
+import ar.lamansys.messages.application.product.exception.ProductNotExistsException;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import ar.lamansys.messages.infrastructure.output.entity.Product;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class GetProductInfo {
+
+    private final ProductStorage productStorage;
+    private final AssertProductExists assertProductExists;
+
+    public Optional<Product> run(String productId) throws ProductNotExistsException {
+        assertProductExists.run(productId);
+
+        return productStorage.getProductByProductId(productId);
+    }
 }
