5f5e1161c87399335243b7194c1c0732f4f314be
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 13:19:25 -0300

    EC-192: Agrego assert y exception en la clase AddNewProductToSell

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AddNewProductToSell.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AddNewProductToSell.java
@@ -1,14 +1,13 @@
 package ar.lamansys.messages.application.product;
 
+import ar.lamansys.messages.application.product.exception.InvalidSellerException;
 import ar.lamansys.messages.application.product.exception.ProductExistsException;
 import ar.lamansys.messages.application.user.AssertUserExists;
 import ar.lamansys.messages.application.user.exception.UserNotExistsException;
-import ar.lamansys.messages.domain.addedproduct.NewProductBo;
 import ar.lamansys.messages.domain.product.ProductBo;
 import ar.lamansys.messages.infrastructure.output.ProductStorage;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -16,10 +15,13 @@ public class AddNewProductToSell {
     private final ProductStorage productStorage;
     private final AssertUserExists assertUserExists;
     private final AssertProductNotExists assertProductNotExists;
+    private final AssertUserIsSeller assertUserSeller;
 
-    public void run(ProductBo newProduct) throws UserNotExistsException, ProductExistsException {
+    public void run(String sellerId, ProductBo newProduct) throws UserNotExistsException, ProductExistsException, InvalidSellerException {
         assertProductNotExists.run(newProduct.getProductId());
+        assertUserExists.run(sellerId);
         assertUserExists.run(newProduct.getOwnerId());
+        assertUserSeller.run(sellerId, newProduct.getOwnerId());
 
         productStorage.save(newProduct);
     }
