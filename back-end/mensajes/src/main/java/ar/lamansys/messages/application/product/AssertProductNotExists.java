489c1e5565cf292448b2fe74127603c2d9772014
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 00:53:14 -0300

    EC-179: Creo la clase AssertProductNotExists e implemento su metodo run

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AssertProductNotExists.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AssertProductNotExists.java
@@ -1,2 +1,19 @@
-package ar.lamansys.messages.application.product;public class AssertProductNotExists {
+package ar.lamansys.messages.application.product;
+
+import ar.lamansys.messages.application.product.exception.ProductExistsException;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class AssertProductNotExists {
+
+    private ProductStorage productStorage;
+
+    public void run(String productId) throws ProductExistsException {
+        if ( productStorage.exists(productId) ) {
+            throw new ProductExistsException(productId);
+        }
+    }
 }
