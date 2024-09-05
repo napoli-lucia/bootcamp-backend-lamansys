4b42b082869b3a97d44f51bbbf3274c9da153e42
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-05 16:40:08 -0300

    EC-197: Creo la clase DeleteAllProductsInCart e implemento su metodo run

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/DeleteAllProductsInCart.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/cart/DeleteAllProductsInCart.java
@@ -1,2 +1,28 @@
-package ar.lamansys.messages.application.cart;public class DeleteAllProductsInCart {
+package ar.lamansys.messages.application.cart;
+
+import ar.lamansys.messages.application.cart.exception.CartNotExistsException;
+import ar.lamansys.messages.application.cart.exception.UserNotOwnsCartException;
+import ar.lamansys.messages.application.user.AssertUserExists;
+import ar.lamansys.messages.application.user.exception.UserNotExistsException;
+import ar.lamansys.messages.infrastructure.output.AddedProductStorage;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class DeleteAllProductsInCart {
+    private final AddedProductStorage addedProduct;
+    private final AssertUserExists assertUserExists;
+    private final AssertCartExists assertCartExists;
+    private final AssertUserOwnsCart assertUserOwnsCart;
+
+    public void run(String ownerId, String cartId) throws UserNotExistsException, CartNotExistsException, UserNotOwnsCartException {
+
+        assertUserExists.run(ownerId);
+        assertCartExists.run(cartId);
+        assertUserOwnsCart.run(cartId, ownerId);
+
+        addedProduct.deleteAllByCartId(cartId);
+
+    }
 }
