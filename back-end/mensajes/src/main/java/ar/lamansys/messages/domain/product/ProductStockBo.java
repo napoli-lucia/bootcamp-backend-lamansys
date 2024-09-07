000b8c6ada58137e1ebdcc10ca5608c86f3b5cf9
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-06 21:31:50 -0300

    EC-202: Creo la clase ProductStockBo

--- back-end/mensajes/src/main/java/ar/lamansys/messages/domain/product/ProductStockBo.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/domain/product/ProductStockBo.java
@@ -1,2 +1,14 @@
-package ar.lamansys.messages.domain.product;public class ProductStockBo {
+package ar.lamansys.messages.domain.product;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductStockBo {
+    @JsonProperty("stock")
+    Integer stock;
 }
