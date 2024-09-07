09571734531eafe67446f9a26cedc9dccd8bed7d
Lucia Napoli <lucianapoli15@gmail.com>
2024-09-06 21:28:33 -0300

    EC-201: Creo la clase UpdateProductStock e implemento su metodo run

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/UpdateProductStock.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/UpdateProductStock.java
@@ -1,2 +1,30 @@
-package ar.lamansys.messages.application.product;public class UpdateProductStock {
+package ar.lamansys.messages.application.product;
+
+import ar.lamansys.messages.application.product.exception.InvalidSellerException;
+import ar.lamansys.messages.application.product.exception.ProductNotExistsException;
+import ar.lamansys.messages.application.user.AssertUserExists;
+import ar.lamansys.messages.application.user.exception.UserNotExistsException;
+import ar.lamansys.messages.domain.product.ProductStockBo;
+import ar.lamansys.messages.infrastructure.output.ProductStorage;
+import ar.lamansys.messages.infrastructure.output.entity.Product;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UpdateProductStock {
+    private final ProductStorage productStorage;
+    private final AssertProductExists assertProductExists;
+    private final AssertUserExists assertUserExists;
+    private final AssertUserIsSeller assertUserSeller;
+
+    public void run(String sellerId, String productId, ProductStockBo newStock) throws ProductNotExistsException, UserNotExistsException, InvalidSellerException {
+        assertProductExists.run(productId);
+        assertUserExists.run(sellerId);
+        assertUserSeller.run(sellerId, productStorage.getOwnerByProductId(productId));
+
+        Product product = productStorage.getProductByProductId(productId).get();
+        product.setStock(newStock.getStock());
+        productStorage.updateProduct(product);
+    }
 }
